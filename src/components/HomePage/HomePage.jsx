9b977602b1e61cdcc53fb58202f0ae36cddd3f97
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-21 07:59:37 +0800

    Update HomePage.jsx

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -15,11 +15,10 @@ const HomePage = () => {
     <div className={styles.homePage}>
       <Decoration /> {/* Render the decoration component */}
       <div className={styles.mainContent}>
-        <h1>Manage Your Finances Masterfully!</h1>
+        <h1>
+        </h1>
         <p>
-          ExpenseTracker effortlessly empowers you to take control of your finances! 
-          With intuitive features, it simplifies the process of tracking and managing expenses, 
-          allowing for a stress-free mastery over your financial world.
+          
         </p>
         <AuthNav /> {/* Navigation for Register/Login */}
         <div className={styles.content}>
