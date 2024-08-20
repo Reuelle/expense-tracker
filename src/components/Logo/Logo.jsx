56f855a200abbc90db8398c2b61d09abe685bf35
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-20 15:07:08 +0800

    Update Logo.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,7 +1,6 @@
-// src/components/Logo/Logo.jsx
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import  useAuth  from '../hooks/useAuth'; // Assumed custom hook for authentication status
+import useAuth from '../hooks/useAuth'; // Assumed custom hook for authentication status
 import logoImage from './Logo.png'; // Path to the logo image
 import styles from './Logo.module.css'; // CSS Module for styling
 
@@ -10,17 +9,13 @@ const Logo = () => {
   const { isAuthenticated } = useAuth(); // Use authentication hook to check if user is authenticated
 
   const handleClick = () => {
-    if (isAuthenticated) {
-      navigate('/path'); // Redirect to MainTransactionPage for authenticated users
-    } else {
-       navigate('/somepath'); // Redirect to HomePage for unauthenticated users
-    }
+    navigate('/'); // Navigate to HomePage
   };
 
   return (
     <div className={styles.logoContainer} onClick={handleClick}>
       <img src={logoImage} alt="Expense Tracker Logo" className={styles.logo} />
-        <span className="logo-text"></span>
+      <span className="logo-text"></span>
     </div>
   );
 };
