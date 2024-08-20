33df5846805b35f8af2cc1197be113127d291d8f
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-20 14:13:38 +0800

    Update Decoration.jsx

--- src/components/Decoration/Decoration.jsx
+++ src/components/Decoration/Decoration.jsx
@@ -1,13 +1,16 @@
 // src/components/DecorationComponent/DecorationComponent.jsx
 import React from 'react';
 import styles from './DecorationComponent.module.css';
+import backgroundImg from '../../assets/Rectangle1.png'; // Import the image
 
 const DecorationComponent = () => (
   <div className={styles.container}>
-    <div className={styles.backgroundImage}></div>
+    <div 
+      className={styles.backgroundImage} 
+      style={{ backgroundImage: `url(${backgroundImg})` }} // Use the imported image
+    ></div>
     <div className={styles.decorationTab}>Decoration Tab</div>
   </div>
 );
 
 export default DecorationComponent;
-
