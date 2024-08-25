03e0504b8e3846992f493082f642f9ae41d95530
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-25 14:13:57 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import GlobalStyles from './styles/GlobalStyles';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header'; // Import Header
-import HomePage from './components/HomePage/HomePage';
-import LoginPage from './components/LoginPage/LoginPage';
+const HomePage = lazy(() => import('./components/HomePage/HomePage'));
+const LoginPage = lazy(() => import('./components/LoginPage/LoginPage'));
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import AuthorizationPage from './components/AuthorizationPage/AuthorizationPage';
 import TransactionHistoryPage from './components/TransactionHistoryPage/TransactionHistoryPage';
@@ -11,16 +11,18 @@ import MainTransactionPage from './components/MainTransactionPage/MainTransactio
 
 const App = () => (
   <Router basename="/expense-tracker">
-    <Header /> {/* Ensure Header is only rendered here */}
-    <Routes>
-      <Route path="/" element={<HomePage />} />
+    <Header />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/authorization" element={<AuthorizationPage />} />
-      <Route path="/transaction-history" element={<TransactionHistoryPage />} />
-      <Route path="/main-transaction" element={<MainTransactionPage />} />
-    </Routes>
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/authorization" element={<AuthorizationPage />} />
+        <Route path="/transaction-history" element={<TransactionHistoryPage />} />
+        <Route path="/main-transaction" element={<MainTransactionPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
   </Router>
-);
-
+  );
 export default App;
