d8e77ba61fb594ef881a57d459c4f07cc9513bf6
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-25 14:15:50 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
-import React, { Suspense, lazy } from 'react';
-import GlobalStyles from './styles/GlobalStyles';
+import React from 'react';
+import GlobalStyles from './styles/GlobalStyles'; // Make sure to use GlobalStyles or remove it if not used
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header/Header'; // Import Header
-const HomePage = lazy(() => import('./components/HomePage/HomePage'));
-const LoginPage = lazy(() => import('./components/LoginPage/LoginPage'));
+import Header from './components/Header/Header';
+import HomePage from './components/HomePage/HomePage';
+import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import AuthorizationPage from './components/AuthorizationPage/AuthorizationPage';
 import TransactionHistoryPage from './components/TransactionHistoryPage/TransactionHistoryPage';
 import MainTransactionPage from './components/MainTransactionPage/MainTransactionPage';
+// Import NotFoundPage if you decide to use it
 
 const App = () => (
   <Router basename="/expense-tracker">
+    <GlobalStyles /> {/* Use GlobalStyles here if applicable */}
     <Header />
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/authorization" element={<AuthorizationPage />} />
-        <Route path="/transaction-history" element={<TransactionHistoryPage />} />
-        <Route path="/main-transaction" element={<MainTransactionPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </Suspense>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/authorization" element={<AuthorizationPage />} />
+      <Route path="/transaction-history" element={<TransactionHistoryPage />} />
+      <Route path="/main-transaction" element={<MainTransactionPage />} />
+      {/* Add a NotFoundPage route if you implement it */}
+      {/* <Route path="*" element={<NotFoundPage />} /> */}
+    </Routes>
   </Router>
-  );
+);
+
 export default App;
