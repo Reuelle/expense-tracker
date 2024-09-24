77f680eff030c020ecde1dab2b12241440d149ed
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-09-24 15:54:54 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import GlobalStyles from './styles/GlobalStyles'; // Make sure to use GlobalStyles or remove it if not used
+import React, { useState } from 'react';
+import GlobalStyles from './styles/GlobalStyles'; 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import HomePage from './components/HomePage/HomePage';
@@ -8,23 +8,33 @@ import RegisterPage from './components/RegisterPage/RegisterPage';
 import AuthorizationPage from './components/AuthorizationPage/AuthorizationPage';
 import TransactionHistoryPage from './components/TransactionHistoryPage/TransactionHistoryPage';
 import MainTransactionPage from './components/MainTransactionPage/MainTransactionPage';
-// Import NotFoundPage if you decide to use it
+import ExpenseChart from './components/ExpenseChart/ExpenseChart'; // Import your ExpenseChart
 
-const App = () => (
-  <Router basename="/expense-tracker">
-    <GlobalStyles /> {/* Use GlobalStyles here if applicable */}
-    <Header />
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/authorization" element={<AuthorizationPage />} />
-      <Route path="/transaction-history" element={<TransactionHistoryPage />} />
-      <Route path="/main-transaction" element={<MainTransactionPage />} />
-      {/* Add a NotFoundPage route if you implement it */}
-      {/* <Route path="*" element={<NotFoundPage />} /> */}
-    </Routes>
-  </Router>
-);
+const App = () => {
+  const [transactions, setTransactions] = useState([]); // State to hold transactions
+
+  const addTransaction = (transaction) => {
+    setTransactions([...transactions, transaction]); // Function to add a transaction
+  };
+
+  return (
+    <Router basename="/expense-tracker">
+      <GlobalStyles />
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/authorization" element={<AuthorizationPage />} />
+        <Route path="/transaction-history" element={<TransactionHistoryPage />} />
+        <Route path="/main-transaction" element={<MainTransactionPage addTransaction={addTransaction} />} />
+        {/* Route to include ExpenseChart */}
+        <Route path="/dashboard" element={<ExpenseChart transactions={transactions} />} />
+        {/* Add a NotFoundPage route if you implement it */}
+        {/* <Route path="*" element={<NotFoundPage />} /> */}
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
